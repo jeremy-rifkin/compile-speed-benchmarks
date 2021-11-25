eb336c0e8a9a14d9103101b889109b5a3ac8fef7
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2021-11-25 13:40:25 -0500

    updated with mongodb debug benchmark

--- benchmark_instructions/mongodb.sh
+++ benchmark_instructions/mongodb.sh
@@ -1,8 +1,19 @@
+# install
 sudo apt install libcurl4-openssl-dev libssl-dev liblzma-dev
 git clone https://github.com/mongodb/mongo.git
 cd mongo
 git checkout tags/r5.1.1-rc0
 python3 -m pip install -r etc/pip/compile-requirements.txt
-time python3 buildscripts/scons.py install-mongod MONGO_VERSION=5.1.1
-rm -rf
+
+# gcc release
+time python3 buildscripts/scons.py install-mongod MONGO_VERSION=5.1.1 --disable-warnings-as-errors
+rm -rf build
+# clang release
 time python3 buildscripts/scons.py install-mongod MONGO_VERSION=5.1.1 CC=clang-12 CXX=clang++-12
+rm -rf build
+# gcc debug
+time python3 buildscripts/scons.py install-mongod --dbg=on MONGO_VERSION=5.1.1 --disable-warnings-as-errors
+rm -rf build
+# clang debug
+time python3 buildscripts/scons.py install-mongod --dbg=on MONGO_VERSION=5.1.1 CC=clang-12 CXX=clang++-12
+rm -rf build
--- benchmark_instructions/opencv.sh
+++ benchmark_instructions/opencv.sh
@@ -1,8 +1,10 @@
+# install
 git clone https://github.com/opencv/opencv.git --depth 1
 #git -C opencv checkout master
 sudo apt install cmake
 mkdir -p build && cd build
 # diagnose problems: VERBOSE=1 with make
+
 # gcc release
 cmake ../opencv -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc-10 -DCMAKE_CXX_COMPILER=g++-10
 time make -j$(nproc)
